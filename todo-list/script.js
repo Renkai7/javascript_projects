65053a1809755d67700658277ce3507caf49d3e0
Michael Spencer <ms53150@gmail.com>
2024-01-13 18:21:22 -0500

    setup To do item class

--- todo-list/script.js
+++ todo-list/script.js
@@ -2,11 +2,44 @@
 
 // Elements
 const taskListContainer = document.querySelector("#task-list");
+const userNewTaskInput = document.querySelector("#new-task");
+const btnAddTask = document.querySelector("#add-task");
 
 // Data
 const tasks = ["Buy", "Cook", "Wash"];
 
-tasks.forEach(function (task) {
-	const html = `<li>${task}</li>`;
-	taskListContainer.insertAdjacentHTML("beforeend", html);
+class ToDoItem {
+	constructor(title) {
+		this.title = title;
+		this.completed = false;
+	}
+
+	// Set item status to completed
+	markCompleted() {
+		this.completed = true;
+	}
+
+	// Remove completed status
+	markIncomplete() {
+		this.completed = false;
+	}
+}
+
+const clearInputField = function () {
+	userNewTaskInput.value = "";
+};
+
+const displayTasks = function () {};
+
+const addTask = function () {
+	tasks.push(userNewTaskInput.value);
+};
+
+addTask();
+
+btnAddTask.addEventListener("click", function (e) {
+	e.preventDefault();
+	addTask();
+	displayTasks();
+	clearInputField();
 });
