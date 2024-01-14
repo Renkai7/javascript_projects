d7f343e0753cd7c10a4994b033ea14374ba66bf4
Michael Spencer <ms53150@gmail.com>
2024-01-13 23:25:04 -0500

    refactor toggle complete method and add functions for adding and displaying items

--- todo-list/script.js
+++ todo-list/script.js
@@ -10,51 +10,66 @@ const tasks = ["Buy", "Cook", "Wash"];
 
 // Classes
 class ToDoItem {
-	constructor(title) {
+	constructor(id, title) {
+		this.id = id;
 		this.title = title;
 		this.completed = false;
 	}
 
-	// Set item status to completed
-	markCompleted() {
-		this.completed = true;
-	}
-
-	// Remove completed status
-	markIncomplete() {
-		this.completed = false;
+	// Toggle completed status
+	toggleComplete() {
+		this.completed = !this.completed;
 	}
 }
 
 class ToDoList {
 	constructor() {
-		this.todoItems = [];
+		this.todoItems = new Map();
 	}
 
 	// add items to todo list
 	addItem(item) {
-		this.todoItems.push(item);
+		this.todoItems.set(item.id, item);
 	}
 
 	// remove item from list
-	removeItem(itemToRemove) {
-		const indexOfItem = this.todoItems.indexOf(itemToRemove);
-		if (indexOfItem === -1) {
-			// TODO: convert this into an error message in html
-			console.log("Item could not be found.");
-			return null;
+	removeItem(itemId) {
+		if (!this.todoItems.has(itemId)) {
+			throw new Error("Item with the specified ID could not be found");
 		}
-		this.todoItems.splice(indexOfItem, 1);
+		this.todoItems.delete(itemId);
 	}
 
 	// Display all items in todo list
 	listAllItems() {
-		this.todoItems.forEach((item) => {
-			console.log(item.title, item.completed);
-		});
+		return Array.from(this.todoItems.values());
 	}
 }
 
+// Functions
+// Add items to the list
+const addItemsToList = function (id, task) {
+	todoList.addItem(new ToDoItem(id, task));
+};
+
+// Display items
+const displayItemsToList = function () {
+	const itemsArray = todoList.listAllItems();
+	itemsArray.forEach((item) => {
+		// TODO: Replace console.log() with creating HTML list items
+		console.log(
+			`ID: ${item.id}, Title: ${item.title}, Completed: ${item.completed}`
+		);
+	});
+};
+
+// TESTING AREA - DELETE LATA
+let todoList = new ToDoList();
+addItemsToList(1, "Learn JavaScript");
+addItemsToList(2, "Write Code");
+
+// Getting the array of todo items
+displayItemsToList();
 // ////////////////////////
 const clearInputField = function () {
 	userNewTaskInput.value = "";
